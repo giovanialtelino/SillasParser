43713dab391ac50569fd82245662758832598847
giovanialtelino <giovanialtelino@hotmail.com>
2020-07-17 02:57:01 -0300

    moved to json

--- Program.cs
+++ Program.cs
@@ -4,24 +4,54 @@ using System.Linq;
 using HtmlAgilityPack;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Web;
+using System.Text.Json;
 
 namespace SillasParser
 {
+    public class Chat
+    {
+        public string Pk { get; set; }
+        public string Date { get; set; }
+        public string From { get; set; }
+        public string MsgContent { get; set; }
+        public String MsgStatus { get; set; }
+        public string MediaType { get; set; }
+        public string MediaSize { get; set; }
+    }
+
     class Program
     {
         public static string ScrubHtml(string value)
         {
-            var step1 = Regex.Replace(value, @"<[^>]+>|&nbsp;", " ").Trim();
-            var step2 = Regex.Replace(step1, @"\s{2,}", " ");
-            return step2;
+            var step0 = Regex.Replace(value, @"<[^>]+>|&nbsp;", " ").Trim();
+            //var step1 = Regex.Replace(value, @"<[^>]+>|&nbsp;", " ").Trim();
+            //var step2 = Regex.Replace(step1, @"\s{2,}", " ");
+            return step0;
+        }
+
+        public static Chat GenerateChat(List<HtmlNode> s)
+        {
+            var newChat = new Chat
+            {
+                Pk = s[0].InnerText,
+                Date = ScrubHtml(s[1].InnerText),
+                From = s[2].InnerText,
+                MsgContent = s[3].InnerText,
+                MsgStatus = s[4].InnerText,
+                MediaType = s[5].InnerText,
+                MediaSize = s[6].InnerText
+            };
+            return newChat;
         }
 
         static void Main(string[] args)
         {
-            var file = String.Concat(Directory.GetCurrentDirectory(), "/TMob.html");
-            var fullFileName = String.Concat(Directory.GetCurrentDirectory(), "/TMob.csv");
+            var htmlFile = String.Concat(Directory.GetCurrentDirectory(), "/TMob.html");
+            var resultFile = String.Concat(Directory.GetCurrentDirectory(), "/TMob.json");
 
-            using (FileStream fs = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (FileStream fs = File.Open(htmlFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             using (BufferedStream bs = new BufferedStream(fs))
             using (StreamReader sr = new StreamReader(bs))
             {
@@ -31,27 +61,24 @@ namespace SillasParser
                 var h = document.GetElementbyId("thead").Elements("tr").ToList();
                 var x = document.GetElementbyId("tbody").Elements("tr").ToList();
 
-                using (var outputFile = new StreamWriter(fullFileName))
+                using (var outputFile = new StreamWriter(resultFile))
                 {
-                    foreach (HtmlNode node in h)
+                    outputFile.Write("[");
+
+                    for (int i = 0; i < x.Count - 2; i++)
                     {
-                        var s = node.Elements("th").ToList();
-                        foreach (HtmlNode item in s)
-                        {
-                            outputFile.Write(String.Concat(ScrubHtml(item.InnerText), ","));
-                        }
-                        outputFile.WriteLine();
+                        var s = x[i].Elements("td").ToList();
+                        var newChat = GenerateChat(s);
+                        outputFile.Write(JsonSerializer.Serialize(newChat));
+                        outputFile.Write(", ");
                     }
 
-                    foreach (HtmlNode node in x)
-                    {
-                        var s = node.Elements("td").ToList();
-                        foreach (HtmlNode item in s)
-                        {
-                            outputFile.Write(String.Concat(ScrubHtml(item.InnerText), ","));
-                        }
-                        outputFile.WriteLine();
-                    }
+                    var f = x[x.Count - 1].Elements("td").ToList();
+                    var finalChat = GenerateChat(f);
+                    outputFile.Write(JsonSerializer.Serialize(finalChat));
+
+                    outputFile.Write("]");
+
                 }
             }
         }
